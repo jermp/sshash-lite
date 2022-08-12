062f72008212baa5affda5d92f1d7fd24f83d505
Giulio Ermanno Pibiri <jeis90@gmail.com>
2022-08-12 16:07:46 +0200

    refactored streaming_query.hpp

--- include/query/streaming_query.hpp
+++ include/query/streaming_query.hpp
@@ -9,127 +9,89 @@
 
 namespace sshash {
 
-inline bool is_positive_read(uint64_t num_positive_kmers_in_read, uint64_t num_kmers_in_read,
-                             float threshold) {
-    return num_positive_kmers_in_read > num_kmers_in_read * threshold;
-}
+template <typename Query, bool FASTQ>
+streaming_query_report streaming_query_from_fastx_file(dictionary const* dict, std::istream& is,
+                                                       double threshold) {
+    /*
 
-template <typename Query>
-streaming_query_report streaming_query_from_fasta_file_multiline(dictionary const* dict,
-                                                                 std::istream& is,
-                                                                 float threshold) {
+    We assume the query file is well-formed, that is:
+    - there are exactly 2 lines per query in case of .fasta file
+      (one header line, one read line);
+    - there are exactly 4 lines per query in case of .fastq file
+      (one header line, one read line, '+' line, quality score line').
+
+    If the flag "SSHASH_QUERY_VERBOSE_OUTPUT" is defined
+    (compile the code with -DSSHASH_QUERY_VERBOSE_OUTPUT=On),
+    then we print the pair (read-id, answer) for each read.
+
+    */
+
     streaming_query_report report;
-    buffered_lines_iterator it(is);
-    std::string buffer;
+    std::string line;
     uint64_t k = dict->k();
     Query query(dict);
-    query.start();
-    uint64_t num_positive_kmers_in_read = 0;
-    uint64_t num_kmers_in_read = 0;
-    while (!it.eof()) {
-        bool empty_line_was_read = it.fill_buffer(buffer);
-        num_kmers_in_read += buffer.size() - k + 1;
-        for (uint64_t i = 0; i != buffer.size() - k + 1; ++i) {
-            char const* kmer = buffer.data() + i;
+
+    while (!is.eof()) {
+        query.start();
+        std::getline(is, line);  // skip first header line
+        std::getline(is, line);
+        if (line.size() < k) continue;
+
+        uint64_t num_positive_kmers_in_read = 0;
+        uint64_t num_negative_kmers_in_read = 0;
+        uint64_t num_kmers_in_read = line.size() - k + 1;
+        uint64_t min_num_positive_kmers_in_read =
+            static_cast<double>(num_kmers_in_read) * threshold;
+        uint64_t max_num_negative_kmers_in_read =
+            num_kmers_in_read - min_num_positive_kmers_in_read;
+
+#ifdef SSHASH_QUERY_VERBOSE_OUTPUT
+        std::cerr << report.num_reads << '\t';
+#endif
+
+        for (uint64_t i = 0; i != num_kmers_in_read; ++i) {
+            char const* kmer = line.data() + i;
             bool answer = query.is_member(kmer);
-            report.num_positive_kmers += answer;
-            num_positive_kmers_in_read += answer;
-        }
-        report.num_kmers += buffer.size() - k + 1;
-        if (empty_line_was_read) { /* re-start the kmers' buffer */
-            report.num_reads += 1;
-            report.num_positive_reads +=
-                is_positive_read(num_positive_kmers_in_read, num_kmers_in_read, threshold);
-            num_positive_kmers_in_read = 0;
-            num_kmers_in_read = 0;
-            buffer.clear();
-            query.start();
-        } else {
-            if (buffer.size() > k - 1) {
-                std::copy(buffer.data() + buffer.size() - k + 1, buffer.data() + buffer.size(),
-                          buffer.data());
-                buffer.resize(k - 1);
+            if (answer) {
+                num_positive_kmers_in_read += 1;
+                report.num_positive_kmers += 1;
+                if (num_positive_kmers_in_read >= min_num_positive_kmers_in_read) {
+                    report.num_positive_reads += 1;
+#ifdef SSHASH_QUERY_VERBOSE_OUTPUT
+                    std::cerr << '1';
+                    std::cerr << '\n';
+#endif
+                    break;
+                }
+            } else {
+                num_negative_kmers_in_read += 1;
+                if (num_negative_kmers_in_read >= max_num_negative_kmers_in_read) {
+#ifdef SSHASH_QUERY_VERBOSE_OUTPUT
+                    std::cerr << '0';
+                    std::cerr << '\n';
+#endif
+                    break;
+                }
             }
         }
-    }
-    report.num_searches = query.num_searches();
-    report.num_extensions = query.num_extensions();
-    return report;
-}
 
-template <typename Query>
-streaming_query_report streaming_query_from_fasta_file(dictionary const* dict, std::istream& is,
-                                                       float threshold) {
-    streaming_query_report report;
-    std::string line;
-    uint64_t k = dict->k();
-    Query query(dict);
-    while (!is.eof()) {
-        query.start();
-        std::getline(is, line);  // skip first header line
-        std::getline(is, line);
-        if (line.size() < k) continue;
-        uint64_t num_positive_kmers_in_read = 0;
-        uint64_t num_kmers_in_read = line.size() - k + 1;
-        for (uint64_t i = 0; i != num_kmers_in_read; ++i) {
-            char const* kmer = line.data() + i;
-            bool answer = query.is_member(kmer);
-            report.num_positive_kmers += answer;
-            num_positive_kmers_in_read += answer;
-        }
         report.num_reads += 1;
-        report.num_positive_reads +=
-            is_positive_read(num_positive_kmers_in_read, num_kmers_in_read, threshold);
         report.num_kmers += num_kmers_in_read;
-    }
-    report.num_searches = query.num_searches();
-    report.num_extensions = query.num_extensions();
-    return report;
-}
 
-template <typename Query>
-streaming_query_report streaming_query_from_fastq_file(dictionary const* dict, std::istream& is,
-                                                       float threshold) {
-    streaming_query_report report;
-    std::string line;
-    uint64_t k = dict->k();
-    Query query(dict);
-    while (!is.eof()) {
-        query.start();
-        /* We assume the file is well-formed, i.e., there are exactly 4 lines per read. */
-        std::getline(is, line);  // skip first header line
-        std::getline(is, line);
-        if (line.size() < k) continue;
-        uint64_t num_positive_kmers_in_read = 0;
-        uint64_t num_kmers_in_read = line.size() - k + 1;
-        for (uint64_t i = 0; i != num_kmers_in_read; ++i) {
-            char const* kmer = line.data() + i;
-            bool answer = query.is_member(kmer);
-            report.num_positive_kmers += answer;
-            num_positive_kmers_in_read += answer;
+        if constexpr (FASTQ) {
+            std::getline(is, line);  // skip '+'
+            std::getline(is, line);  // skip score
         }
-        report.num_reads += 1;
-        report.num_positive_reads +=
-            is_positive_read(num_positive_kmers_in_read, num_kmers_in_read, threshold);
-        report.num_kmers += num_kmers_in_read;
-        std::getline(is, line);  // skip '+'
-        std::getline(is, line);  // skip score
     }
+
     report.num_searches = query.num_searches();
     report.num_extensions = query.num_extensions();
-    return report;
-}
 
-template <typename Query>
-streaming_query_report streaming_query_from_fasta_file(dictionary const* dict, std::istream& is,
-                                                       bool multiline, float threshold) {
-    if (multiline) return streaming_query_from_fasta_file_multiline<Query>(dict, is, threshold);
-    return streaming_query_from_fasta_file<Query>(dict, is, threshold);
+    return report;
 }
 
 streaming_query_report dictionary::streaming_query_from_file(std::string const& filename,
-                                                             bool multiline,
-                                                             float threshold) const {
+                                                             double threshold) const {
     std::ifstream is(filename.c_str());
     if (!is.good()) throw std::runtime_error("error in opening the file '" + filename + "'");
     streaming_query_report report;
@@ -137,44 +99,36 @@ streaming_query_report dictionary::streaming_query_from_file(std::string const& 
     if (util::ends_with(filename, ".fa.gz") or util::ends_with(filename, ".fasta.gz")) {
         zip_istream zis(is);
         if (canonicalized()) {
-            report = streaming_query_from_fasta_file<streaming_query_canonical_parsing>(
-                this, zis, multiline, threshold);
+            report = streaming_query_from_fastx_file<streaming_query_canonical_parsing, false>(
+                this, zis, threshold);
         } else {
-            report = streaming_query_from_fasta_file<streaming_query_regular_parsing>(
-                this, zis, multiline, threshold);
+            report = streaming_query_from_fastx_file<streaming_query_regular_parsing, false>(
+                this, zis, threshold);
         }
     } else if (util::ends_with(filename, ".fq.gz") or util::ends_with(filename, ".fastq.gz")) {
-        if (multiline) {
-            std::cout << "==> Warning: option 'multiline' is only valid for FASTA files, not FASTQ."
-                      << std::endl;
-        }
         zip_istream zis(is);
         if (canonicalized()) {
-            report = streaming_query_from_fastq_file<streaming_query_canonical_parsing>(this, zis,
-                                                                                        threshold);
+            report = streaming_query_from_fastx_file<streaming_query_canonical_parsing, true>(
+                this, zis, threshold);
         } else {
-            report = streaming_query_from_fastq_file<streaming_query_regular_parsing>(this, zis,
-                                                                                      threshold);
+            report = streaming_query_from_fastx_file<streaming_query_regular_parsing, true>(
+                this, zis, threshold);
         }
     } else if (util::ends_with(filename, ".fa") or util::ends_with(filename, ".fasta")) {
         if (canonicalized()) {
-            report = streaming_query_from_fasta_file<streaming_query_canonical_parsing>(
-                this, is, multiline, threshold);
+            report = streaming_query_from_fastx_file<streaming_query_canonical_parsing, false>(
+                this, is, threshold);
         } else {
-            report = streaming_query_from_fasta_file<streaming_query_regular_parsing>(
-                this, is, multiline, threshold);
+            report = streaming_query_from_fastx_file<streaming_query_regular_parsing, false>(
+                this, is, threshold);
         }
     } else if (util::ends_with(filename, ".fq") or util::ends_with(filename, ".fastq")) {
-        if (multiline) {
-            std::cout << "==> Warning: option 'multiline' is only valid for FASTA files, not FASTQ."
-                      << std::endl;
-        }
         if (canonicalized()) {
-            report = streaming_query_from_fastq_file<streaming_query_canonical_parsing>(this, is,
-                                                                                        threshold);
+            report = streaming_query_from_fastx_file<streaming_query_canonical_parsing, true>(
+                this, is, threshold);
         } else {
-            report = streaming_query_from_fastq_file<streaming_query_regular_parsing>(this, is,
-                                                                                      threshold);
+            report = streaming_query_from_fastx_file<streaming_query_regular_parsing, true>(
+                this, is, threshold);
         }
     }
 
--- src/query.cpp
+++ src/query.cpp
@@ -11,16 +11,11 @@ int main(int argc, char** argv) {
     parser.add("index_filename", "Must be a file generated with src/build.cpp");
     parser.add("query_filename",
                "Must be a FASTA/FASTQ file (.fa/fasta or .fq/fastq extension) compressed with gzip "
-               "or not.");
-    parser.add("multiline",
-               "Use this option if more the one DNA line must be parsed after each header."
-               " Only valid for FASTA files (not FASTQ).",
-               "--multiline", true);
+               "or not. It is expected one read per line.");
     parser.add("threshold",
                "A real value in [0,1] indicating the requested minimal fraction of positive k-mers "
                "per read. Default value is 0.0.",
                "-t", false);
-    parser.add("print_index_info", "Print index information.", "--print-index-info", true);
     if (!parser.parse()) return 1;
 
     auto index_filename = parser.get<std::string>("index_filename");
@@ -31,16 +26,14 @@ int main(int argc, char** argv) {
     uint64_t num_bytes_read = essentials::load(dict, index_filename.c_str());
     std::cout << "index size: " << essentials::convert(num_bytes_read, essentials::MB) << " [MB] ("
               << (num_bytes_read * 8.0) / dict.size() << " [bits/kmer])" << std::endl;
-    if (parser.get<bool>("print_index_info")) dict.print_info();
 
-    bool multiline = parser.get<bool>("multiline");
-    float threshold = 0.0;
-    if (parser.parsed("threshold")) threshold = parser.get<float>("threshold");
+    double threshold = 0.0;
+    if (parser.parsed("threshold")) threshold = parser.get<double>("threshold");
 
     essentials::logger("performing queries from file '" + query_filename + "'...");
     essentials::timer<std::chrono::high_resolution_clock, std::chrono::microseconds> t;
     t.start();
-    auto report = dict.streaming_query_from_file(query_filename, multiline, threshold);
+    auto report = dict.streaming_query_from_file(query_filename, threshold);
     t.stop();
     essentials::logger("DONE");
 
