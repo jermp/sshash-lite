f65997a2fbffdfa1c11f5be11d0c3398e0a775be
Giulio Ermanno Pibiri <jeis90@gmail.com>
2022-08-13 19:20:52 +0200

    single executable

--- README.md
+++ README.md
@@ -10,11 +10,11 @@ build the dictionaries, and data format, please refer to [this](https://github.c
 
 Build an index over the E. Coli matchtigs with
 
-	./build ../data/ecoli_matchtigs_k31.fa.gz 31 15 -d tmp --check -o ecoli-matchtigs.sshash
+	./sshash-lite build ../data/ecoli_matchtigs_k31.fa.gz 31 15 -d tmp --check -o ecoli-matchtigs.sshash
 
 and query with
 
-	./query ecoli-matchtigs.sshash ../data/queries/SRR9873306_1.10K.fastq.gz -t 0.8
+	./sshash-lite query ecoli-matchtigs.sshash ../data/queries/SRR9873306_1.10K.fastq.gz -t 0.8
 
 where a query read is considered as positive if at least 80% of the k-mers are positive.
 
@@ -27,3 +27,9 @@ To print the result for each query, define the flag `SSHASH_QUERY_VERBOSE_OUTPUT
 	make -j
     
 and re-run the query.
+
+The query output is written on `std::cerr` by default, so it is possible to capture the output with
+
+	./sshash-lite query ecoli-matchtigs.sshash ../data/queries/SRR9873306_1.10K.fastq.gz -t 0.8 2> query_output.txt
+	
+which writes the output to the file `query_output.txt`.
--- src/sshash-lite.cpp
+++ src/sshash-lite.cpp
@@ -2,11 +2,12 @@
 
 #include "../external/pthash/external/cmd_line_parser/include/parser.hpp"
 #include "../include/dictionary.hpp"
+#include "../include/query/streaming_query.hpp"
 #include "check_utils.hpp"
 
 using namespace sshash;
 
-int main(int argc, char** argv) {
+int build(int argc, char** argv) {
     cmd_line_parser::parser parser(argc, argv);
 
     /* mandatory arguments */
@@ -15,7 +16,7 @@ int main(int argc, char** argv) {
                "\twith one DNA sequence per line.\n"
                "\tFor example, it could be the de Bruijn graph topology output by BCALM (without "
                "duplicates)\n"
-               "\tof matchtigs (with duplicates).");
+               "\tor matchtigs (with duplicates).");
     parser.add("k", "K-mer length (must be <= " + std::to_string(constants::max_k) + ").");
     parser.add("m", "Minimizer length (must be < k).");
 
@@ -85,4 +86,74 @@ int main(int argc, char** argv) {
     }
 
     return 0;
+}
+
+int query(int argc, char** argv) {
+    cmd_line_parser::parser parser(argc, argv);
+    parser.add("index_filename", "Must be a file generated with src/build.cpp");
+    parser.add("query_filename",
+               "Must be a FASTA/FASTQ file (.fa/fasta or .fq/fastq extension) compressed with gzip "
+               "or not. It is expected one read per line.");
+    parser.add("threshold",
+               "A real value in [0,1] indicating the requested minimal fraction of positive k-mers "
+               "per read. Default value is 0.0.",
+               "-t", false);
+    if (!parser.parse()) return 1;
+
+    auto index_filename = parser.get<std::string>("index_filename");
+    auto query_filename = parser.get<std::string>("query_filename");
+
+    dictionary dict;
+    essentials::logger("loading index from file '" + index_filename + "'...");
+    uint64_t num_bytes_read = essentials::load(dict, index_filename.c_str());
+    std::cout << "index size: " << essentials::convert(num_bytes_read, essentials::MB) << " [MB] ("
+              << (num_bytes_read * 8.0) / dict.size() << " [bits/kmer])" << std::endl;
+
+    double threshold = 0.0;
+    if (parser.parsed("threshold")) threshold = parser.get<double>("threshold");
+
+    essentials::logger("performing queries from file '" + query_filename + "'...");
+    essentials::timer<std::chrono::high_resolution_clock, std::chrono::microseconds> t;
+    t.start();
+    auto report = dict.streaming_query_from_file(query_filename, threshold);
+    t.stop();
+    essentials::logger("DONE");
+
+    std::cout << "==== query report:\n";
+    std::cout << "num_kmers = " << report.num_kmers << std::endl;
+    std::cout << "num_positive_kmers = " << report.num_positive_kmers << " ("
+              << (report.num_positive_kmers * 100.0) / report.num_kmers << "%)" << std::endl;
+    std::cout << "num_reads = " << report.num_reads << std::endl;
+    std::cout << "num_positive_reads (" << threshold << ") = " << report.num_positive_reads << " ("
+              << (report.num_positive_reads * 100.0) / report.num_reads << "%)" << std::endl;
+    std::cout << "num_searches = " << report.num_searches << "/" << report.num_positive_kmers
+              << " (" << (report.num_searches * 100.0) / report.num_positive_kmers << "%)"
+              << std::endl;
+    std::cout << "num_extensions = " << report.num_extensions << "/" << report.num_positive_kmers
+              << " (" << (report.num_extensions * 100.0) / report.num_positive_kmers << "%)"
+              << std::endl;
+    std::cout << "elapsed = " << t.elapsed() / 1000 << " millisec / ";
+    std::cout << t.elapsed() / 1000000 << " sec / ";
+    std::cout << t.elapsed() / 1000000 / 60 << " min / ";
+    std::cout << (t.elapsed() * 1000) / report.num_kmers << " ns/kmer" << std::endl;
+
+    return 0;
+}
+
+void help() {
+    std::cerr << "Supported commands are:\n";
+    std::cerr << "sshash-lite build\n";
+    std::cerr << "sshash-lite query\n";
+}
+
+int main(int argc, char** argv) {
+    auto cmd = std::string(argv[1]);
+    if (cmd == "build") {
+        return build(argc - 1, argv + 1);
+    } else if (cmd == "query") {
+        return query(argc - 1, argv + 1);
+    }
+    std::cerr << "Unsupported command '" << cmd << "'.";
+    help();
+    return 1;
 }
